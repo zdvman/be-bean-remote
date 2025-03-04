fe41465e80d8a07e4f208d00408aefffaace052c
TasosPat <tasos.pat@outlook.com>
2025-03-04 15:02:18 +0000

    getCafeByID complete

--- src/routes/cafes-router.js
+++ src/routes/cafes-router.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const cafesRouter = express.Router();
 const {
-  getCafes
+  getCafes,
+  getCafeByID
 } = require('../controllers/cafes.controllers');
 const { authMiddleware, restrictTo } = require('../middleware/auth'); // Import both middleware
 
 cafesRouter.route('/').get(getCafes);
+cafesRouter.route('/:id').get(getCafeByID);
 
 // Protected route: Get user by username (requires authentication)
 // cafesRouter.route('/:username').get(authMiddleware, getUserByUsername);
