a99b4643fb818b26fc1afe5e171e184162efb791
TasosPat <tasos.pat@outlook.com>
2025-03-04 15:02:18 +0000

    getCafeByID complete

--- src/models/cafes.models.js
+++ src/models/cafes.models.js
@@ -7,6 +7,21 @@ function selectCafes() {
     });
 }
 
+function selectCafeByID(cafe_id) {
+    const sql = `SELECT * FROM cafes WHERE id=$1`;
+    const args = [cafe_id]
+    return db.query(sql, args).then(({ rows }) => {
+        if (!rows[0]) {
+            return Promise.reject({
+              status: 404,
+              msg: `No cafe found for cafe_id: ${cafe_id}`
+            });
+          }
+      return rows[0];
+    });
+}
+
 module.exports = {
-  selectCafes
+  selectCafes,
+  selectCafeByID
 };
