e36c9efbfa613a8b1216cf13b95e1979080ce3a9
TasosPat <tasos.pat@outlook.com>
2025-03-05 11:44:52 +0000

    getAmenitiesByCafeID done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -288,7 +288,8 @@ describe("GET /cafes", () => {
   });
 });
 
-describe("GET /cafes/:cafe_id", () => {
+describe('GET /cafes/:cafe_id', () => {
+
   test("200: Responds with a single cafe", () => {
     return request(app)
       .get("/api/cafes/1")
@@ -296,13 +297,14 @@ describe("GET /cafes/:cafe_id", () => {
       .then((response) => {
         const cafe = response.body.cafe;
         expect(cafe.owner_id).toBe(2);
-        expect(cafe.name).toBe("Remote Bean Central");
+        expect(cafe.name).toBe('Remote Bean Central');
         expect(cafe.description).toBe("A cozy cafe for remote workers");
         expect(cafe.address).toBe("123 Coffee St, Manchester");
         expect(cafe.busy_status).toBe("quiet");
         expect(cafe.is_verified).toBe(false);
       });
   });
+
   test("GET:404 sends an appropriate status and error message when given a valid but non-existent id", () => {
     return request(app)
       .get("/api/cafes/999")
@@ -319,4 +321,68 @@ describe("GET /cafes/:cafe_id", () => {
         expect(response.body.msg).toBe("Bad request");
       });
   });
+});
+
+  
+
+describe("GET /api/cafes sorting queries",() => {
+  test("200: Responds with all cafes with a certain amenity", () => {
+    return request(app)
+    .get('/api/cafes?amenity=WiFi')
+    .expect(200)
+    .then(({body}) => {
+      expect(body.cafes).toBeInstanceOf(Array);
+      expect(body.cafes.length).toBeGreaterThan(0);
+      body.cafes.forEach((cafe) => {
+        expect(cafe).toMatchObject({
+          owner_id: expect.any(Number),
+          name: expect.any(String),
+          description: expect.any(String),
+          address: expect.any(String),
+          location: expect.any(String),
+          busy_status: expect.any(String),
+          is_verified: expect.any(Boolean)
+        });
+      });
+    })
+  });
+  test("404: returns an error 404 when no cafes have this amenity", () => {
+    return request(app)
+    .get('/api/cafes?amenity=Outdoor%20Seating')
+    .expect(404)
+      .then((response) => {
+        expect(response.body.msg).toBe('No cafes with this amenity');
+      });
+  })
+});
+
+describe("GET /api/cafes/:cafe_id/amenities",() => {
+  test("200: Responds with all amenities of a certain cafe", () => {
+    const result = {
+      cafe_id: '1',
+      amenities: [ { name: 'WiFi' }, { name: 'Power Outlets' } ]
+    };
+    return request(app)
+    .get('/api/cafes/1/amenities')
+    .expect(200)
+    .then(({body}) => {
+      expect(body).toEqual(result)
+    })
+  });
+  test('GET:404 sends an appropriate status and error message when given a valid but non-existent id', () => {
+  return request(app)
+    .get('/api/cafes/999/amenities')
+    .expect(404)
+    .then((response) => {
+      expect(response.body.msg).toBe('No cafe found for cafe_id: 999');
+    });
+});
+test('GET:400 sends an appropriate status and error message when given an invalid id', () => {
+  return request(app)
+    .get('/api/cafes/not-a-cafe/amenities')
+    .expect(400)
+    .then((response) => {
+      expect(response.body.msg).toBe('Bad request');
+    });
+});
 });
--- src/controllers/cafes.controllers.js
+++ src/controllers/cafes.controllers.js
@@ -1,4 +1,4 @@
-const { selectCafes, selectCafeByID } = require('../models/cafes.models');
+const { selectCafes, selectCafeByID, selectCafesByAmenity, selectAmenitiesByCafeId } = require('../models/cafes.models');
 
 function getCafes(req, res, next) {
   return selectCafes()
@@ -17,7 +17,34 @@ function getCafeByID(req, res, next) {
     .catch(next);
 }
 
+function getCafesByAmenity(req, res, next) {
+    const amenity = req.query.amenity;
+    return selectCafesByAmenity(amenity)
+    .then((cafes) => {
+        if (cafes.length === 0) {
+            return res.status(404).send({ msg: 'No cafes with this amenity' });
+          }
+          res.status(200).json({ cafes });
+    })
+    .catch(next);
+}
+
+function getAmenitiesByCafeId(req, res, next) {
+    const { id } = req.params;
+    selectCafeByID(id)
+    .then(() => {
+        return selectAmenitiesByCafeId(id)
+    })
+    .then((amenities) => {
+        const cafe_id = id;
+      res.status(200).send({ cafe_id, amenities});
+    })
+    .catch(next);
+}
+
 module.exports = {
   getCafes,
-  getCafeByID
+  getCafeByID,
+  getCafesByAmenity,
+  getAmenitiesByCafeId
 };
