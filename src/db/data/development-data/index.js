0e08ee787e9cc91a52ffee6f5c04f29ea32d0642
zdvman <zuyevdmytro@gmail.com>
2025-03-09 16:06:52 +0000

    add dev data for testing and endpoints.json

--- src/db/data/development-data/index.js
+++ src/db/data/development-data/index.js
@@ -1,1 +1,18 @@
-exports.userData = require('./users.js');
+exports.usersData = require('./users.js');
+exports.userPreferencesData = require('./user_preferences.js');
+exports.userFavoritesData = require('./user_favorites.js');
+
+exports.cafesData = require('./cafes.js');
+exports.cafeQrCodesData = require('./cafe_qr_codes.js');
+exports.cafeMediaData = require('./cafe_media.js');
+exports.cafeAmenitiesData = require('./cafe_amenities.js');
+exports.cafeVisitsData = require('./cafe_visits.js');
+
+exports.amenitiesData = require('./amenities.js');
+
+exports.reviewsData = require('./reviews.js');
+exports.reviewVotesData = require('./review_votes.js');
+exports.reviewResponsesData = require('./review_responses.js');
+
+exports.adminData = require('./admins.js');
+exports.reportsData = require('./reports.js');
